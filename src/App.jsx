d15c58ee5f318420e5b0d2f3a955a19a14f2e8cd
Janaina Oliveira <janainaon1@gmail.com>
2022-08-17 08:33:11 -0300

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import "./assets/css/base/base.css";
+
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Pagina404 from "./pages/Pagina404";
+import Cabecalho from "./components/Cabecalho";
 
 function App() {
   return (
-  <>
-
-  </>
-  )
+    <BrowserRouter>
+      <Cabecalho />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/sobre'>
+          <Sobre />
+        </Route>
+        <Route>
+          <Pagina404 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
